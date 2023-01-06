58ea4de782aa52ae98ec5f1b408060ffcd48af29
xiaobin <481996467@qq.com>
2023-01-07 01:33:25 +0800

    不饿

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+	transpileDependencies: true,
+	lintOnSave: false, // 关闭ESlint的语法检测
 })
